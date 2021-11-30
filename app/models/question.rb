dc5fb45c0f2edbfe4af14809dfd67d4ff03f1625
anonymous anonymous <pjpauljung@gmail.com>
2021-11-30 22:26:41 +0000

    Direct association between users and questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,9 @@
 class Question < ApplicationRecord
   # Direct associations
 
+  belongs_to :creator,
+             :class_name => "User"
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :created_questions,
+             :class_name => "Question",
+             :foreign_key => "creator_id",
+             :dependent => :destroy
+
   has_many   :votes,
              :foreign_key => "voter_id",
              :dependent => :destroy
