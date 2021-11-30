6c9c5eda07e73c45c756cb959ff783ac5d5421a5
anonymous anonymous <pjpauljung@gmail.com>
2021-11-30 22:26:58 +0000

    Whitelist columns in QuestionSession for ActiveAdmin

--- app/admin/question_sessions.rb
+++ app/admin/question_sessions.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register QuestionSession do
 
+ permit_params :interviewee_id, :company_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
