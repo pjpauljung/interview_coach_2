a51dd35033e2bbfd671dd44f20c56addafe5fcbc
anonymous anonymous <pjpauljung@gmail.com>
2021-11-30 22:27:34 +0000

    Only question user can modify question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  before_action :current_user_must_be_question_creator, only: [:edit, :update, :destroy] 
+
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   # GET /questions
@@ -59,6 +61,14 @@ class QuestionsController < ApplicationController
 
 
   private
+
+  def current_user_must_be_question_creator
+    set_question
+    unless current_user == @question.creator
+      redirect_back fallback_location: root_path, alert: "You are not authorized for that."
+    end
+  end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_question
       @question = Question.find(params[:id])
